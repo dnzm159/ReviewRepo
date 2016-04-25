fc8a5340fc9c21cc1b10d7304ac979ff9997d2e7
Administrator <Administrator@305-23>
2016-04-25 11:22:21 +0900

    multi기능추가

--- src/test/java/reviewproj/ReviewCalcTest.java
+++ src/test/java/reviewproj/ReviewCalcTest.java
@@ -9,8 +9,16 @@ public class ReviewCalcTest {
 	@Test
 	public void testAdd() {
 		CalcReview r = new CalcReview();
-		r.add(10,  20);
+		r.add(10,20);
 		assertEquals(30, r.getResult());	
+	}
+	
+	@Test
+	public void testMulti()
+	{
+		CalcReview r = new CalcReview();
+		r.multi(10,3);
+		assertEquals(10,r.getResult());
 	}
 
 }
